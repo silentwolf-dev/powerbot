27e09cf71b28b60fdd06f314194151eb17234547
silentwolf-dev <omariimanibrown421@gmail.com>
2021-08-16 21:46:41 -0400

    start up

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
+from require import require
 import discord
+from require import require
 from discord.ext import commands
 
 
+# get the token
+Token = require('./json/secret.json')['bot'].get('token')
+
 bot = commands.Bot(command_prefix='!')
 
-
+## events
 @bot.event
 async def on_ready(): 
-    pass
+    print('bot is ready')
+
+
+
+
+
+bot.run(Token)
+
